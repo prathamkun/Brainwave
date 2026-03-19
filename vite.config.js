8e3cba04e08149eb42b8d3d280d01a61d841df08
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-03-19 22:11:19 +0530

    deleted

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-plugins: [react(), tailwindcss()];
-
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-});
