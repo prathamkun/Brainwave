49db0cfcc27586c358d9b6efeaa3d8160197cb3f
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-03-27 20:23:25 +0530

    classes updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <img src={assets.logo} alt="logo" className='w-32 sm:w-44'  onClick={()=>
             navigate('/')
         }/>
-        <button>Get started <ArrowRight className=''/></button>
+        <button className='flex items-center gap-2 rounded-full text-sm cursor-poinnter bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button>
 
     </div>
   )
