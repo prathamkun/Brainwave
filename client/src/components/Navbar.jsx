8806297e4bc23df6adf51c414a588c836d985685
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-04-03 20:42:31 +0530

    navbar update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,13 @@ const Navbar = () => {
           user ? <UserButton /> 
           : 
           (
-            <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button>
+            <button 
+  onClick={openSignIn}
+  className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'
+>
+  Get started 
+  <ArrowRight className='w-4 h-4'/>
+</button>
 
           )
         }
